73d292b04de4a9fdcbf005195719ee065556f023
imcoco <imcoco@gmail.com>
2022-06-17 20:19:24 -0400

    initialize mongoose

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+import "dotenv/config.js"
 import createError from 'http-errors'
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
-
+import('./config/database.js')
 // import routers
 import { router as indexRouter } from './routes/index.js'
 import { router as usersRouter } from './routes/users.js'
